aeb588156f8e9cab7f64582f928233008871cf8a
Inu Ganteng <102247436+MR-Xyaa@users.noreply.github.com>
2022-04-03 07:07:11 +0700

    Create mulai.sh

---
+++ mulai.sh
@@ -0,0 +1,30 @@
+#!/system/xbin/bash/sh/python
+#MauNgapainGblk?
+#DiRecodeGakBakalJadiinLuMastah
+#DiGantiAuthorGakBakalJadiinLuMaster
+#MikirMakeOtakGblk
+#KontolKaloMauReedit/Recode 
+#Pm Gua Bangsat
+
+
+cd module
+clear
+python loading.py
+sleep 5
+clear
+biru='\033[34;1m'
+ijo='\033[32;1m'  
+ungu='\033[35;1m'
+bm='\033[36;1m'
+abang='\033[31;1m'
+putih='\033[37;1m'                                           
+kuning='\033[33;1m'
+sleep 1
+
+
+toilet -f slant "Spamer";
+echo -e $ijo"1. Spam Whatsaap"
+echo -e $bm"2. Spam Call"
+
+read -p "Pilih No:" bro
+if [ $bro = 1 ] || [ $bro = 1 ]
